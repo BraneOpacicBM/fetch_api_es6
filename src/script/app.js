1ebe48fcf5151c2ca3380c2b69a979be4aa24a39
Brane Opacic <brane.opacic@brightmarbles.io>
2018-04-02 15:13:15 +0100

    Created most of the js logic work and added basic styling of posts on index.html

--- src/script/app.js
+++ src/script/app.js
@@ -1,3 +1,49 @@
 import '../assets/styles/main.scss';
+import {urlPosts, urlUsers, div} from './modules/vars';
 import {Element} from './modules/element';
 
+
+let userReq = fetch(urlUsers)
+.then((res) => res.json());
+
+let postReq = fetch(urlPosts)
+.then((res) => res.json());
+
+Promise.all([userReq, postReq])
+        .then((res) => {
+            const users = res[0];
+            const posts = res[1].sort(() => .5 - Math.random()).slice(0, 10);
+            console.log(users);
+            
+            posts.forEach((post) => {
+
+                // create Elements from class Element
+
+                let itemContainer = Element.createNode("div");
+                Element.addClass(itemContainer, "item");
+                let paragTitle = Element.createNode("p");
+                Element.addClass(paragTitle, "title");
+                let paragBody = Element.createNode("p");
+                Element.addClass(paragBody, "content");
+                let paragUser = Element.createNode("p");
+                Element.addClass(paragUser, "user");
+
+                // fill up the content via JSON
+
+                paragTitle.innerHTML = post.title;
+                paragBody.innerHTML = post.body;
+                
+
+
+
+                // append Elements using class Element
+
+                Element.append(itemContainer, paragTitle);
+                Element.append(itemContainer, paragBody);
+                Element.append(itemContainer, paragUser);
+                Element.append(div, itemContainer);
+
+
+            });
+        })
+        .catch((err) => console.log(err));
---
+++ src/script/modules/vars.js
@@ -0,0 +1,3 @@
+export const urlPosts = 'https://jsonplaceholder.typicode.com/posts';
+export const urlUsers = 'https://jsonplaceholder.typicode.com/users';
+export let div = document.getElementsByClassName("posts")[0];
